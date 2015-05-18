6b149eda44acb402055ba84d896cd50aa7dc2265
Dan Williams <daniel@Hezekiah.(none)>
2015-05-18 23:19:47 +0100

    Pulled powerup into separate routing Added basic PWM for initial fade-in

--- main.c
+++ main.c
@@ -23,16 +23,10 @@ static int ltr = 1;
 
 /*===========================================================================*/
 
-void main(void) {
 
-	// Initialise PortD as output
-	DDRD = 0xFF;
-	// Start with the middle LED lit
-	PORTD = 0x08;
-	
-	// Wait for dramatic effect
-	// TODO - powerup gradual fade-in rather than instant-on
-	_delay_ms(500);
+int main(void) {
+
+	powerup_2();
 	
 	
 	// Main loop
@@ -49,9 +43,69 @@ void main(void) {
 	}		
 }
 
+
 /*===========================================================================*/
 
-void larson(void) {
+// Second attempt at a powerup routine. This one uses an inelegant PWM to
+// gradually increase the perceived brightness of a single LED before main()
+// calls larson() to begin scanning.
+int powerup_2(void) {
+
+	// Initialise PortD as output
+	DDRD = 0xFF;
+	
+	// Set to no prescaling
+	TCCR1B |= (1 << CS10);
+	
+	// Duty cycle is approximately 60Hz
+	// Begin with LED lit for 0 ticks
+	// Step size of 50 ticks	TODO - implement logarithmic step?
+	int duty = 16000;
+	int on = 0;
+	int step = 50;
+	
+	// PWM loop until we are at full brightness
+	while (on<=duty) {
+	
+		if (TCNT1 < on) 			// When count is within the "on" time
+		{
+			PORTD |= 0x08;			// Light the LED
+		}
+		else if (TCNT1 < duty) 		// When count is outside the "on" time
+		{		
+			PORTD &= !0x08;			// Dim the LED
+		}
+		else						// When the count has reached the duty cycle limit
+		{
+			TCNT1 = 0;				// Reset the counter
+			on = on + step;			// Increase the time for which the LED is to be lit
+		}
+	}
+
+}
+
+/*===========================================================================*/
+
+// Initial powerup routing to switch on a single LED and wait before
+// starting to scan.
+int powerup_1(void) {
+
+	// Initialise PortD as output
+	DDRD = 0xFF;
+	// Start with the middle LED lit
+	PORTD = 0x08;
+	
+	// Wait for dramatic effect
+	// TODO - powerup gradual fade-in rather than instant-on
+	_delay_ms(500);	
+
+}
+
+
+/*===========================================================================*/
+
+
+int larson(void) {
 // TODO - rewrite this to shift a PWM pattern
 
 	if (ltr==1) 					// Scan left to right
