b4b71ff8009ec2c3eae527573176b81922bcce67
Dan Williams <daniel@Hezekiah.(none)>
2015-05-21 17:41:58 +0100

    powerup Sorted out logarithmic PWM fade in Still some bugs to iron out, but currently working

--- main.c
+++ main.c
@@ -19,28 +19,11 @@
 // Roughly 60Hz duty cycle
 const int duty = 16000;
 
-
-
-// Create 256 step log look-up table for PWM stages
+// Create 128 step log look-up table for PWM stages
 // log_step[i] = duty*[1-log(i,duty)]
-const int num_steps = 256;
-const int log_step[num_steps] = {	
-	0, 11, 23, 34, 45, 57, 68, 80, 92, 103, 115, 127, 139, 150, 162, 174, 
-	186, 198, 210, 223, 235, 247, 259, 272, 284, 297, 309, 322, 334, 347, 360, 372, 
-	385, 398, 411, 424, 437, 450, 464, 477, 490, 504, 517, 531, 544, 558, 572, 585, 
-	599, 613, 627, 641, 655, 669, 684, 698, 712, 727, 741, 756, 771, 785, 800, 815, 
-	830, 845, 860, 876, 891, 906, 922, 937, 953, 969, 984, 1000, 1016, 1032, 1049, 1065, 
-	1081, 1098, 1114, 1131, 1147, 1164, 1181, 1198, 1215, 1233, 1250, 1267, 1285, 1303, 1320, 1338, 
-	1356, 1374, 1392, 1411, 1429, 1448, 1466, 1485, 1504, 1523, 1542, 1562, 1581, 1601, 1620, 1640, 
-	1660, 1680, 1701, 1721, 1741, 1762, 1783, 1804, 1825, 1846, 1868, 1889, 1911, 1933, 1955, 1978, 
-	2000, 2023, 2045, 2068, 2092, 2115, 2139, 2162, 2186, 2210, 2235, 2259, 2284, 2309, 2334, 2360, 
-	2385, 2411, 2437, 2464, 2490, 2517, 2544, 2572, 2599, 2627, 2655, 2684, 2712, 2741, 2771, 2800, 
-	2830, 2860, 2891, 2922, 2953, 2984, 3016, 3049, 3081, 3114, 3147, 3181, 3215, 3250, 3285, 3320, 
-	3356, 3392, 3429, 3466, 3504, 3542, 3581, 3620, 3660, 3701, 3741, 3783, 3825, 3868, 3911, 3955, 
-	4000, 4045, 4092, 4139, 4186, 4235, 4284, 4334, 4385, 4437, 4490, 4544, 4599, 4655, 4712, 4771, 
-	4830, 4891, 4953, 5016, 5081, 5147, 5215, 5285, 5356, 5429, 5504, 5581, 5660, 5741, 5825, 5911, 
-	6000, 6092, 6186, 6284, 6385, 6490, 6599, 6712, 6830, 6953, 7081, 7215, 7356, 7504, 7660, 7825, 
-	8000, 8186, 8385, 8599, 8830, 9081, 9356, 9660, 10000, 10385, 10830, 11356, 12000, 12830, 14000, 16000};
+const int num_steps = 127;
+const int log_step[128] = {	
+	0, 26, 52, 79, 106, 133, 160, 187, 215, 243, 271, 299, 328, 357, 386, 415, 445, 475, 505, 535, 566, 597, 628, 660, 692, 724, 757, 789, 823, 856, 890, 924, 959, 994, 1029, 1065, 1101, 1137, 1174, 1212, 1249, 1288, 1326, 1365, 1405, 1445, 1485, 1526, 1568, 1610, 1653, 1696, 1740, 1784, 1829, 1874, 1921, 1968, 2015, 2063, 2112, 2162, 2212, 2264, 2316, 2368, 2422, 2477, 2532, 2589, 2646, 2705, 2764, 2825, 2886, 2949, 3013, 3079, 3146, 3214, 3283, 3354, 3427, 3501, 3577, 3655, 3734, 3816, 3900, 3985, 4073, 4164, 4257, 4353, 4451, 4553, 4658, 4766, 4878, 4994, 5114, 5239, 5368, 5503, 5644, 5791, 5944, 6105, 6275, 6453, 6642, 6842, 7056, 7283, 7528, 7793, 8080, 8395, 8743, 9132, 9573, 10082, 10684, 11421, 12371, 13711, 16000};
 
 
 // Begin left-to-right
@@ -53,7 +36,7 @@ static int ltr = 1;
 
 int main(void) {
 
-	powerup_3();
+	powerup();
 	
 	
 	// Main loop
@@ -73,10 +56,10 @@ int main(void) {
 
 /*===========================================================================*/
 
-// Third attempt at a powerup routine with a logarithmic step change.
+// Powerup routine with a logarithmic step change.
 // Each loop runs until timer count == 1/duty seconds (ie each loop takes 1/60s)
 // Thus, for 256 steps, the loop takes approx 4.27s to reach peak brightness.
-int powerup_3(void) {
+int powerup(void) {
 
 	// Initialise PortD as output
 	DDRD = 0xFF;
@@ -91,70 +74,21 @@ int powerup_3(void) {
 	while (i<num_steps) {
 		
 		if (TCNT1 < log_step[i]) {		// When timer count is within the "on" time
-			PORTD |= 0x08;			// Light the LED
+			PORTD |= 0x08;				// Light the LED
 		}
 		else if (TCNT1 < duty) {		// When timer count is outside the "on" time
-			PORTD &= !0x08;			// Dim the LED
+			PORTD &= !0x08;				// Dim the LED
 		}
-		else {					// When the count has reached the duty cycle limit
-			TCNT1 = 0;			// Reset the timer count
-			i++;				// Move to next PWM width
-		}
+		else {							// When the count has reached the duty cycle limit
+			TCNT1 = 0;					// Reset the timer count
+			i++;						// Move to next PWM width
+		}	
 	} 
+	
+	_delay_ms(2000);					// TODO - interrupt here
+										// TODO - sort out flicker here
+	
 }
-/*===========================================================================*/
-
-// Second attempt at a powerup routine. This one uses an inelegant PWM to
-// gradually increase the perceived brightness of a single LED before main()
-// calls larson() to begin scanning.
-int powerup_2(void) {
-
-	// Initialise PortD as output
-	DDRD = 0xFF;
-	
-	// Set to no prescaling
-	TCCR1B |= (1 << CS10);
-	
-	// Duty cycle is approximately 60Hz
-	// Begin with LED lit for 0 ticks
-	// Step size of 50 ticks	TODO - implement logarithmic step?
-	int duty = 16000;
-	int on = 0;
-	int step = 10;
-	
-	// PWM loop until we are at full brightness
-	while (on<=duty) {
-	
-		if (TCNT1 < on) {			// When count is within the "on" time
-			PORTD |= 0x08;			// Light the LED
-		}
-		else if (TCNT1 < duty) {		// When count is outside the "on" time
-			PORTD &= !0x08;			// Dim the LED
-		}
-		else {					// When the count has reached the duty cycle limit
-			TCNT1 = 0;			// Reset the counter
-			on = on + step;			// Increase the time for which the LED is to be lit
-		}
-	}
-}
-
-/*===========================================================================*/
-
-// Initial powerup routing to switch on a single LED and wait before
-// starting to scan.
-int powerup_1(void) {
-
-	// Initialise PortD as output
-	DDRD = 0xFF;
-	// Start with the middle LED lit
-	PORTD = 0x08;
-	
-	// Wait for dramatic effect
-	// TODO - powerup gradual fade-in rather than instant-on
-	_delay_ms(500);	
-
-}
-
 
 /*===========================================================================*/
 
