7f7680daef57befadefa33ed29f5b7727384106d
spitfire23bc <spitfire23bc@gmail.com>
2015-05-22 16:49:00 +0100

    Friday Club 22/05/15
    Added interrupt handling for PWM.
    Whole pattern sweeps.

--- main.c
+++ main.c
@@ -14,61 +14,109 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>		// TODO - remove this once delays are done with interrupts
 
+
+/*===========================================================================*/
+
+int powerup(void);
+int larson(void);
+int led_control(void);
+
 /*===========================================================================*/
 
 // Roughly 60Hz duty cycle
 const int duty = 16000;
 
 // Create 128 step log look-up table for PWM stages
-// log_step[i] = duty*[1-log(i,duty)]
-const int num_steps = 127;
-const int log_step[128] = {	
-	0, 26, 52, 79, 106, 133, 160, 187, 215, 243, 271, 299, 328, 357, 386, 415, 445, 475, 505, 535, 566, 597, 628, 660, 692, 724, 757, 789, 823, 856, 890, 924, 959, 994, 1029, 1065, 1101, 1137, 1174, 1212, 1249, 1288, 1326, 1365, 1405, 1445, 1485, 1526, 1568, 1610, 1653, 1696, 1740, 1784, 1829, 1874, 1921, 1968, 2015, 2063, 2112, 2162, 2212, 2264, 2316, 2368, 2422, 2477, 2532, 2589, 2646, 2705, 2764, 2825, 2886, 2949, 3013, 3079, 3146, 3214, 3283, 3354, 3427, 3501, 3577, 3655, 3734, 3816, 3900, 3985, 4073, 4164, 4257, 4353, 4451, 4553, 4658, 4766, 4878, 4994, 5114, 5239, 5368, 5503, 5644, 5791, 5944, 6105, 6275, 6453, 6642, 6842, 7056, 7283, 7528, 7793, 8080, 8395, 8743, 9132, 9573, 10082, 10684, 11421, 12371, 13711, 16000};
+// log_step[i] = duty*[1-log(i,num_steps+1)]
+const int num_steps = 32;
+const uint16_t log_step[32] = {	
+	0, 147, 298, 454, 616, 784, 959, 1140, 
+	1328, 1525, 1730, 1945, 2170, 2407, 2656, 2920, 
+	3200, 3498, 3816, 4159, 4528, 4930, 5370, 5856, 
+	6400, 7016, 7728, 8570, 9600, 10928, 12800, 16000};
 
 
 // Begin left-to-right
 // Declare as static so larson() knows which way to shift the register
 static int ltr = 1;			
 
+volatile uint8_t led;
 
 /*===========================================================================*/
 
 
 int main(void) {
 
-	powerup();
+	sei();
+	TIMSK = (1 << OCIE1A) | (1 << OCIE1B);
+
+	// Initialise PortD as output
+	DDRD = 0xFF;
+
+
+	//powerup();
 	
-	
+	led = 0x08;
+	led_control();
 	// Main loop
 	while(1) {
-
+		
 
 		// Delay between LED changes
 		// TODO - implement the delay with interrupts
 		_delay_ms(100);
 		
+		
+		PORTD = led;
+		
 		// Update the PORTD register to shift the lit LED
 		larson();
+
 		
-	}		
+	}	
+	
+		
 }
 
 
+/*===========================================================================*/
+int led_control(void) {
+	
+	// Set to no prescaling
+	TCCR1B |= (1 << CS10);
+	TCCR1B |= (1 << WGM12); // Configure timer 1 for CTC mode
+	OCR1A = 16000;
+	OCR1B = 14000;
+	
+	return 1;
+	
+}
+
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTD &= ~((led<<1) | (led>>1));		// LEDs off
+	PORTD |= led;
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+	PORTD |= (led<<1) | (led>>1);		// LEDs on
+	PORTD |= led;
+}
 /*===========================================================================*/
 
 // Powerup routine with a logarithmic step change.
 // Each loop runs until timer count == 1/duty seconds (ie each loop takes 1/60s)
 // Thus, for 256 steps, the loop takes approx 4.27s to reach peak brightness.
 int powerup(void) {
-
-	// Initialise PortD as output
-	DDRD = 0xFF;
 	
 	// Set to no prescaling
 	TCCR1B |= (1 << CS10);
 	
 	// Begin with LED lit for 0 ticks
 	int i = 0;
+	int j = 0;
 
 	// PWM loop until we are at full brightness
 	while (i<num_steps) {
@@ -77,17 +125,22 @@ int powerup(void) {
 			PORTD |= 0x08;				// Light the LED
 		}
 		else if (TCNT1 < duty) {		// When timer count is outside the "on" time
-			PORTD &= !0x08;				// Dim the LED
+			PORTD &= ~0x08;				// Dim the LED
 		}
 		else {							// When the count has reached the duty cycle limit
 			TCNT1 = 0;					// Reset the timer count
-			i++;						// Move to next PWM width
+			j++;
+			if (j>=5) {
+				i++;					// Move to next PWM width
+				j=0;
+			}						
 		}	
 	} 
 	
 	_delay_ms(2000);					// TODO - interrupt here
 										// TODO - sort out flicker here
 	
+	return 1;
 }
 
 /*===========================================================================*/
@@ -98,20 +151,22 @@ int larson(void) {
 
 	if (ltr==1) 						// Scan left to right
 	{
-		if (PORTD<64) {
-			PORTD = (PORTD << 1);			// Left-shift to light next LED
+		if (led<64) {
+			led = (led << 1);			// Left-shift to light next LED
 		}
 		else {				 		// Right-most position		
 			ltr = 0;				// Set the direction to right-to-left
 		}
 	}
 	else {							// Scan right to left 
-		if (PORTD>1) {
-			PORTD = (PORTD >> 1);			// Right-shift to light next LED
+		if (led>1) {
+			led = (led >> 1);			// Right-shift to light next LED
 		}
 		else {						// Left-most position
 			ltr = 1;				// Set the direction to left-to-right
 		}
 	}
+
+	return 1;
 
 }
