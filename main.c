ab2815bdba91c3c7fc697181056d11dd32dbf027
Dan Williams <daniel@Hezekiah.(none)>
2015-06-07 23:27:25 +0100

    Fade-in profiles Added log profiles for fade-in of scanner and thruster LEDs Tidied up some code

--- main.c
+++ main.c
@@ -22,7 +22,6 @@
  *             0V  10 --|      |-- 11  scan LED7
  *                       ------
  *
- *	TODO - add flickering thrusters
  *	TODO - tidy up interrupt code and fade-in
  *
  */
@@ -39,6 +38,8 @@ typedef enum { false, true } bool_t;
 /*===========================================================================*/
 // Prototypes
 int main(void);
+int thr_control(void);
+int scan_control(void);
 int larson(void);
 
 /*===========================================================================*/
@@ -58,17 +59,21 @@ const uint8_t con_scan_bitsB = 0x0F;
 const uint8_t con_thr_bitsB = 0x30;
 const uint8_t con_wep_bitsB = 0x80;
 
-const uint16_t thr_array1[16] = {8000, 8000, 7000, 7000, 6000, 6000, 5000, 5000, 6000, 6000, 7000, 7000, 8000, 8000, 9000, 9000};
-const uint16_t thr_array2[16] = {6000, 6000, 7000, 7000, 8000, 8000, 9000, 9000, 8000, 8000, 7000, 7000, 6000, 6000, 5000, 5000};
+// Delays
+const uint8_t con_scan_delay = 31;
+
+const uint8_t scan_array[40] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 5, 7, 9, 11, 13, 16, 18, 21, 24, 28, 31, 35, 39, 43, 48, 53, 60, 67, 75, 85, 98, 117, 149, 255};
+
+const uint16_t thr_array[64] = {106, 220, 343, 475, 619, 776, 950, 1145, 1366, 1621, 1922, 2291, 2766, 3436, 4582, 6317, 8388, 10378, 13038, 15000, 16000, 15000, 14200, 14000, 14500, 15500, 16000, 14500, 13500, 14500, 15500, 15000, 13500, 13000, 13000, 14000, 13000, 12500, 13500, 14000, 14000, 15000, 14000, 14500, 15000, 15500, 16000, 15000, 16000, 15000, 14000, 13000, 14000, 14000, 14000, 15000, 14000, 15000, 14000, 13000, 13000, 11500, 12500, 14000};
 
 /*===========================================================================*/
 // Global signals
 
 // Begin left-to-right
-static bool_t scan_ltr = true;			
+volatile bool_t scan_ltr = true;			
 
-// Current lit LED - intialised to the middle LED
-static uint16_t scan_led = 0x00;
+// Current lit LED
+volatile uint16_t scan_led = 0x00;
 
 // Strings to contain scanner and thruster LED on/off commands
 volatile uint16_t scan_string = 0x0000;
@@ -76,11 +81,12 @@ volatile uint8_t thr_string = 0x00;
 uint8_t portb_string = 0x00;
 
 // Counters
-volatile uint8_t i = 0;
-volatile uint8_t j = 0;
-volatile uint8_t k = 0;
-volatile uint8_t l = 0;
-volatile uint8_t m = 0;
+volatile uint8_t scan_out_pos = 0;
+volatile uint8_t scan_mid_pos = 15;
+volatile uint8_t scan_delay = 0;
+volatile uint8_t thr_pos_A = 0;
+volatile uint8_t thr_pos_B = 0;
+volatile uint8_t wait_to_scan= 0;
 
 
 /*===========================================================================*/
@@ -95,15 +101,13 @@ int main(void) {
 	//        t0 compA    |   t0 compB    |  t0 overflow |   t1 compA    |   t1 compB
 	TIMSK = (1 << OCIE0A) | (1 << OCIE0B) | (1 << TOIE0) | (1 << OCIE1A) | (1 << OCIE1B);
 	
-	OCR1A = 500;
-	OCR1B = 500;
-	
 	/*-----------------------------------------------------------------------*/
 
 	// Initialise PORTB and PORTD as outputs
 	DDRB = 0xFF;
 	DDRD = 0xFF;
 	scan_led = con_scan_LED_mid;
+	
 	/*-----------------------------------------------------------------------*/
 
 	// Setup timer0 to overflow at approx 60Hz (8bit timer / 64 = 16320 ticks)
@@ -136,62 +140,8 @@ int main(void) {
 
 // timer0 overflow interrupt - occurs at ~60Hz
 ISR(TIMER0_OVF_vect) {
-
-	// Reset timer1 (16 bit timer)
-	TCNT1 = 0;
-	// Switch on thruster LEDs
-	thr_string = con_thr_1 | con_thr_2;
-	
-	if (l<15) {
-		l++;
-	}
-	else {
-		l=0;
-	}		
-	OCR1A = thr_array1[l];
-	
-	if (m>0) {
-		m--;
-	}
-	else {
-		m=15;
-	}
-	OCR1B = thr_array2[m];	
-
-
-
-	// Scanner fade-in
-	if (OCR0B<245) {
-		if (j >= 3) {
-			OCR0A++;
-			OCR0B += 10;
-			j=0;
-		}
-		else {
-			j++;
-		}
-	}
-	else {
-		// Disable timer0 compare B interrupt once at full brightness
-		TIMSK &= ~(1 << OCIE0B);
-		
-		if (k >= 63) {
-			i++;
-		}
-		else {
-			k++;
-		}
-		
-	}
-
-
-	if (i>=6) {
-		// Call larson() to shift the lit LED demand
-		larson();
-		i=0;
-	}
-	
-	scan_string |= (scan_led<<1) | scan_led | (scan_led>>1);
+	thr_control();
+	scan_control();
 }
 
 /*===========================================================================*/
@@ -205,7 +155,7 @@ ISR(TIMER0_COMPA_vect) {
 /*===========================================================================*/
 
 // timer0 compare B interrupt
-// PWM off for inner LED (disabled after startup)
+// PWM off for mid LED (disabled after startup)
 ISR(TIMER0_COMPB_vect) {
 	scan_string &= ~scan_led;
 }
@@ -224,6 +174,83 @@ ISR(TIMER1_COMPA_vect) {
 // 
 ISR(TIMER1_COMPB_vect) {
 	thr_string &= ~con_thr_2;
+}
+
+/*===========================================================================*/
+
+int thr_control(void) {
+
+	// Reset timer1 (16 bit timer)
+	TCNT1 = 0;
+	
+	// Thruster LEDs PWM on
+	thr_string = con_thr_1 | con_thr_2;
+	
+	
+	// Select timer1 compA value
+	if (thr_pos_A<23) {
+		thr_pos_A++;
+	}
+	else if (thr_pos_A<63) {
+		thr_pos_A++;
+	}
+	else {
+		thr_pos_A=23;
+	}		
+	OCR1A = thr_array[thr_pos_A];
+	
+	
+	// Select timer1 compB value
+	if (thr_pos_B<23) {
+		thr_pos_B++;
+	}
+	else if (thr_pos_B>23) {
+		thr_pos_B--;
+	}
+	else {
+		thr_pos_B=63;
+	}		
+	OCR1B = thr_array[thr_pos_B];
+
+	return 1;
+}
+
+/*===========================================================================*/
+
+int scan_control(void) {
+
+	// Scanner fade-in
+	if (scan_mid_pos<38) {
+			scan_mid_pos++;		
+			scan_out_pos++;	
+			OCR0A = scan_array[scan_out_pos];
+			OCR0B = scan_array[scan_mid_pos];
+	}
+	else {
+		// Disable timer0 compare B interrupt once at full brightness
+		TIMSK &= ~(1 << OCIE0B);
+		
+		/*-------------------------------------------------------------------*/
+		// Call larson() after an initial delay to shift the lit LED demand
+		
+		if (wait_to_scan >= con_scan_delay) {
+			scan_delay++;
+		}
+		else {
+			wait_to_scan++;
+		}
+	}
+
+	if (scan_delay>=6) {
+
+		larson();
+		scan_delay=0;
+	}
+	
+	// Scanner mid and outer LEDs PWM on
+	scan_string |= (scan_led<<1) | scan_led | (scan_led>>1);
+
+	return 1;
 }
 
 /*===========================================================================*/
